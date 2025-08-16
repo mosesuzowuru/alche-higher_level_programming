b272aac89801d404109a4e5d23643c3e1d45c627
mosesuzowuru <c.uzowuru@alustudent.com>
2025-08-16 06:04:07 -0700

    ....

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for i in range(26):
-    letter = chr(ord('a') + i)
-    if letter != 'q' and letter != 'e':
-        print(letter, end="")
+    if chr(ord('a') + i) != 'q' and chr(ord('a') + i) != 'e':
+        print("{}".format(chr(ord('a') + i)), end="")
