b6693de4b9e8a749b416343806711d82ffa8edd3
mosesuzowuru <c.uzowuru@alustudent.com>
2025-08-16 05:03:07 -0700

    ....

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Holberton"
-str2 = "School"
-print("Welcome to {str1} {str2}!")
-# This is my code
+str1 = "Welcome to "
+str2 = "Holberton School!"
+str1 = str1 + str2
+print(str1)
