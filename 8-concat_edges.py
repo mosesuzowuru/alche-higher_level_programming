ac52765fe4a526dc5d07e0887a42f7b63d71f32e
mosesuzowuru <c.uzowuru@alustudent.com>
2025-08-16 03:50:42 -0700

    Fixing parts of the script

---
+++ 8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, high-level, general-purpose programming language. Created by Guido van Rossum"
+str = str[39:67] + str[115:119] + str[78:85] + str[106:115]
+print(str)
