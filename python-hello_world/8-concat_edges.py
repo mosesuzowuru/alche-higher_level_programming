36052252f2a2135a228addfb90d0d24ae1c05807
mosesuzowuru <c.uzowuru@alustudent.com>
2025-08-16 04:13:23 -0700

    ....

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, high-level, general-purpose programming language. Created by Guido van Rossum"
-str = str[39:67] + str[115:119] + str[78:85] + str[106:115]
+str = ("Python is an interpreted, high-level, object-oriented, "
+       "general-purpose programming language with dynamic semantics. "
+       "Created by Guido van Rossum")
+str = str[39:54] + str[76:88] + str[101:106] + str[0:6]
 print(str)
