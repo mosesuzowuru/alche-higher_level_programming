58169ed64dd8c823fc481f77002a3d9775d7d9d5
mosesuzowuru <c.uzowuru@alustudent.com>
2025-08-16 05:20:55 -0700

    ...

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = ("Python is an interpreted, high-level, object-oriented "
-       "programming language with dynamic semantics. Created by Guido van Rossum")
-str = str[39:55] + str[56:67] + str[77:82] + str[0:6]
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
 print(str)
+# This is what we do
