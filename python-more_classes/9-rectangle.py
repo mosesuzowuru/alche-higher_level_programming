dc68f4c4e63ab35e15d81a020bf3dab6f57ff1c7
mosesuzowuru <c.uzowuru@alustudent.com>
2025-09-06 09:18:52 -0700

    ...

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -97,7 +97,8 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join(str(self.print_symbol) * self.__width for _ in range(self.__height))
+        symbol = str(self.print_symbol)
+        return "\n".join(symbol * self.__width for _ in range(self.__height))
 
     def __repr__(self):
         """Return string representation to recreate the instance
@@ -130,7 +131,7 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        
+
         if rect_1.area() >= rect_2.area():
             return rect_1
         return rect_2
