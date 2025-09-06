b345d1892a211ca1a1c99f57cb9f20d10edf13a3
mosesuzowuru <c.uzowuru@alustudent.com>
2025-09-06 08:42:18 -0700

    ...

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -97,7 +97,8 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join(str(self.print_symbol) * self.__width for _ in range(self.__height))
+        symbol = str(self.print_symbol)
+        return "\n".join(symbol * self.__width for _ in range(self.__height))
 
     def __repr__(self):
         """Return string representation to recreate the instance
